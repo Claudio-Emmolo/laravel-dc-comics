c590390e05780538117a3b785247c906788202de
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-02-13 14:59:02 +0100

    Print example DB data

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,7 +1,36 @@
 @extends('layouts.app')
 
 @section('main-app')
-    <h1>
-        Hello!
-    </h1>
+<main>
+    <h1>DC Comic List</h1>
+
+    <table class="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Titolo</th>
+            <th scope="col">Prezzo</th>
+            <th scope="col">Serie</th>
+            <th scope="col">Tipo</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+            @forelse ($comicList as $comic)
+            <tr>
+                <th scope="row">{{ $comic->id }}</th>
+                <td>{{ $comic->title }}</td>
+                <td>{{ $comic->price }}</td>
+                <td>{{ $comic->series }}</td>
+                <td>{{ $comic->type }}</td>
+                <td>
+                    <a href="" class="btn btn-success">View</a>
+                </td>
+            </tr>
+            @empty
+                
+            @endforelse
+        </tbody>
+      </table>
+</main>
 @endsection
