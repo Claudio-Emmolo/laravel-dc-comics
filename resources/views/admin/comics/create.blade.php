8320d08dd64c7383347d5bcdeaed5712d29e02fb
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-02-15 15:29:30 +0100

    Create Form template/partials for edit & create

--- resources/views/admin/comics/create.blade.php
+++ resources/views/admin/comics/create.blade.php
@@ -6,39 +6,9 @@
 <main>
     <h2 class="text-center mb-2">Create New Comic</h2>
     <div class="container">
-        <form action="{{ route('admin.comic.store') }}" method="POST">
-            @csrf
-            <div class="mb-3">
-                <label for="title" class="form-label">Title</label>
-                <input type="text" class="form-control" id="title" name="title">
-            </div>
-            <div class="mb-3">
-                <label for="description" class="form-label">description</label>
-                <textarea class="form-control" id="description" name="description" ></textarea>           
-            </div>
-            <div class="mb-3">
-                <label for="thumb" class="form-label">Thumb</label>
-                <textarea class="form-control" id="thumb" name="thumb"></textarea>           
-            </div>
-            <div class="mb-3">
-                <label for="price" class="form-label">Price</label>
-                <input type="number" step="0.01" class="form-control" id="price" name="price">
-            </div>
-            <div class="mb-3">
-                <label for="series" class="form-label">Series</label>
-                <input type="text" class="form-control" id="series" name="series">
-            </div>
-            <div class="mb-3">
-                <label for="sale_date" class="form-label">Sale Date</label>
-                <input type="date" class="form-control" id="sale_date" name="sale_date">
-            </div>
-            <div class="mb-3">
-                <label for="type" class="form-label">Type</label>
-                <input type="text" class="form-control" id="type" name="type">
-            </div>
+        {{-- Import Form --}}
+        @include('admin.comics.partials.form', ["route" => 'admin.comic.store', 'methodRoute' => 'POST'])
 
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
     </div>
 </main>
 @endsection
--- resources/views/admin/comics/edit.blade.php
+++ resources/views/admin/comics/edit.blade.php
@@ -6,42 +6,8 @@
 <main>
     <h2 class="text-center mb-2">Edit Comic</h2>
     <div class="container">
-        <form action="{{ route('admin.comic.update', $comic->id) }}" method="POST">
-            @csrf
-            @method('PUT')
-
-
-            <div class="mb-3">
-                <label for="title" class="form-label">Title</label>
-                <input type="text" class="form-control" id="title" name="title" value="{{$comic->title}}">
-            </div>
-            <div class="mb-3">
-                <label for="description" class="form-label">description</label>
-                <textarea class="form-control" id="description" name="description" >{{$comic->description}}</textarea>           
-            </div>
-            <div class="mb-3">
-                <label for="thumb" class="form-label">Thumb</label>
-                <textarea class="form-control" id="thumb" name="thumb" >{{$comic->thumb}}</textarea>           
-            </div>
-            <div class="mb-3">
-                <label for="price" class="form-label">Price</label>
-                <input type="number" step="0.01" class="form-control" id="price" name="price" value="{{$comic->price}}">
-            </div>
-            <div class="mb-3">
-                <label for="series" class="form-label">Series</label>
-                <input type="text" class="form-control" id="series" name="series" value="{{$comic->series}}">
-            </div>
-            <div class="mb-3">
-                <label for="sale_date" class="form-label">Sale Date</label>
-                <input type="date" class="form-control" id="sale_date" name="sale_date" value="{{$comic->sale_date}}">
-            </div>
-            <div class="mb-3">
-                <label for="type" class="form-label">Type</label>
-                <input type="text" class="form-control" id="type" name="type" value="{{$comic->type}}">
-            </div>
-
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
+        {{-- Import Form --}}
+        @include('admin.comics.partials.form', ["route" => 'admin.comic.update', 'methodRoute' => 'PUT'])
     </div>
 </main>
 @endsection
