f1276a10a156303c9ff4a8be8c9bc4acb04d117c
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-02-16 15:22:41 +0100

    saves the fields that the user has entered in the form

--- resources/views/admin/comics/partials/form.blade.php
+++ resources/views/admin/comics/partials/form.blade.php
@@ -16,31 +16,31 @@
     
     <div class="mb-3">
         <label for="title" class="form-label">Title</label>
-        <input type="text" class="form-control" id="title" name="title" value="{{$comic->title}}">
+        <input type="text" class="form-control" id="title" name="title" value="{{ old('title') ?? $comic->title }}">
     </div>
     <div class="mb-3">
         <label for="description" class="form-label">description</label>
-        <textarea class="form-control" id="description" name="description" >{{$comic->description}}</textarea>           
+        <textarea class="form-control" id="description" name="description">{{  old('description') ?? $comic->description}}</textarea>           
     </div>
     <div class="mb-3">
         <label for="thumb" class="form-label">Thumb</label>
-        <textarea class="form-control" id="thumb" name="thumb" >{{$comic->thumb}}</textarea>           
+        <textarea class="form-control" id="thumb" name="thumb" >{{  old('thumb') ?? $comic->thumb}}</textarea>           
     </div>
     <div class="mb-3">
         <label for="price" class="form-label">Price</label>
-        <input type="number" step="0.01" class="form-control" id="price" name="price" value="{{$comic->price}}">
+        <input type="number" step="0.01" class="form-control" id="price" name="price" value="{{  old('price') ?? $comic->price}}">
     </div>
     <div class="mb-3">
         <label for="series" class="form-label">Series</label>
-        <input type="text" class="form-control" id="series" name="series" value="{{$comic->series}}">
+        <input type="text" class="form-control" id="series" name="series" value="{{ old('series') ?? $comic->series}}">
     </div>
     <div class="mb-3">
         <label for="sale_date" class="form-label">Sale Date</label>
-        <input type="date" class="form-control" id="sale_date" name="sale_date" value="{{$comic->sale_date}}">
+        <input type="date" class="form-control" id="sale_date" name="sale_date" value="{{  old('sale_date') ?? $comic->sale_date}}">
     </div>
     <div class="mb-3">
         <label for="type" class="form-label">Type</label>
-        <input type="text" class="form-control" id="type" name="type" value="{{$comic->type}}">
+        <input type="text" class="form-control" id="type" name="type" value="{{  old('type') ?? $comic->type}}">
     </div>
 
     <button type="submit" class="btn btn-primary">Submit</button>
