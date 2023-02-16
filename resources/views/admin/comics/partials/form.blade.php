b01bc014d84d05d1b3bdaccccbefba15e8d88eed
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-02-16 12:54:58 +0100

    Print Errors on Form

--- resources/views/admin/comics/partials/form.blade.php
+++ resources/views/admin/comics/partials/form.blade.php
@@ -1,6 +1,14 @@
-@foreach ($errors->all() as $error)
-    @dump($error)
-@endforeach
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li>
+                {{$error}}
+            </li>
+            @endforeach
+        </ul>
+    </div>
+@endif
 
 <form action="{{ route($route, $comic->id) }}" method="POST">
     @csrf
